6ad042e4dc9506df605b5314417644495f83f62a
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 05:07:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here! hash in hash with arrays of strings
+  # write your code here! hash in hash with arrays of strings, keys are hash with symbols, values are hashes with keys and arrays of strings. map works with arrays but not hashes.  
   binding.pry
 end
