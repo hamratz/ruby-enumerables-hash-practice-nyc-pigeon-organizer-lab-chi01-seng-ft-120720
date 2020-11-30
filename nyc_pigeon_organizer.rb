b93d8d97e7cdef64fb8d1bd0026ffb0c37cdf898
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 05:08:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,14 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
+  
+  hash.each_with_object({}) do |(k, v), a|
+    a[k] = v + 3
+  end 
+  
+  
+  
+  
+  
   # write your code here! hash in hash with arrays of strings, keys are hash with symbols, values are hashes with keys and arrays of strings. map works with arrays but not hashes.  
   binding.pry
 end
