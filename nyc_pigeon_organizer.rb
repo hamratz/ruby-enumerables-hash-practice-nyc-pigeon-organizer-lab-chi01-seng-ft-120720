84461e18c78ae0507ccdf45b2e1eff01bb5192f3
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 05:20:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     binding.pry
     value.each do |inner_key, names|
+  end
+  binding.pry
     final_array
   end 
   
