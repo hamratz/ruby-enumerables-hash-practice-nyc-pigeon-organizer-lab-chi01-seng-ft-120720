42bff10f24501bb35ce57427f5385cf3b4ace8b3
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 05:12:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key, value), final_array|
-    final_array[k] = v + 3
+    binding.pry
+    final_array
   end 
   
   
