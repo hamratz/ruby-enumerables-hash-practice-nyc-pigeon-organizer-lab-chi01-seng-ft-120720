25ef83dc915d7b9cc814ec5e2f1e0c9372324ba7
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 05:42:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,11 @@
-require 'pry'
+
 def nyc_pigeon_organizer(data)
   
   final_results = data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
- #binding.pry 
+
  if !final_array[name]
    final_array[name] = {}
   end
@@ -17,7 +17,8 @@ end
  end
     #final_array
   end 
-
-  # write your code here! hash in hash with arrays of strings, keys are hash with symbols, values are hashes with keys and arrays of strings. map works with arrays but not hashes.  
-  #binding.pry
 end
+
+
+
+# write your code here! hash in hash with arrays of strings, keys are hash with symbols, values are hashes with keys and arrays of strings. map works with arrays but not hashes.  
